a3bab522cbdbb3c9a255d76529370be75907b96e
senthilnataraj <37336427+senthilnataraj@users.noreply.github.com>
2018-03-20 12:45:49 +0530

    Add files via upload

---
+++ beginner level/odd or even.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      loges
+#
+# Created:     20/03/2018
+# Copyright:   (c) loges 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=(int(input()))
+
+    if(a%2==0):
+        print("value is even")
+    else:
+        print("value is odd")
+
+if __name__ == '__main__':
+    main()
