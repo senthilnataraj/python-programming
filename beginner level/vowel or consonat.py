fb599bca8d577612a8b5431a83ed098548a78c28
senthilnataraj <37336427+senthilnataraj@users.noreply.github.com>
2018-03-20 12:49:20 +0530

    Add files via upload

---
+++ beginner level/vowel or consonat.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      loges
+#
+# Created:     20/03/2018
+# Copyright:   (c) loges 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     list=("a","e","i","o","l","A","E","I","O","L")
+     i=input()
+     if(i in list):
+        print("vowel")
+     else:
+        print("consonat")
+
+if __name__ == '__main__':
+    main()
